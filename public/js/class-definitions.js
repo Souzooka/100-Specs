5edabcd07e397c04f449f4844b9234cef6ee1fae
Dakota <bazookaharwell@gmail.com>
2017-04-08 11:19:27 -1000

    define getType

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1224,6 +1224,28 @@ var dinner = new Meal(['fish', 'vegetables']);
  *
  */
 
+  Shape.prototype.getType = function() {
+    switch (this.sides) {
+      case 3:
+        return 'triangle';
+      case 4:
+        return 'quadrilateral';
+      case 5:
+        return 'pentagon';
+      case 6:
+        return 'hexagon';
+      case 7:
+        return 'heptagon';
+      case 8:
+        return 'octagon';
+      case 9:
+        return 'nonagon';
+      case 10:
+        return 'decagon';
+      default:
+        return 'Could not determine type';
+    }
+  };
 
 /* Step 84
  *
